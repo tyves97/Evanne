f1bfc3206839d591e1da359d8a0e3174f7feb114
tyves97 <tyves97@gmail.com>
2026-08-17 17:53:51 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE = 'cnu-suivi-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './saisie.html',
+  './donnees.html',
+  './rapports.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const network = fetch(e.request)
+        .then((resp) => {
+          if (resp && resp.ok) {
+            const copy = resp.clone();
+            caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+          }
+          return resp;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
